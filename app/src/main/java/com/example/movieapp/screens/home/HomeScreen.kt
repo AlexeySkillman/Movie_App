89f5624e072fe3dbb7ec97b7810a47f7cc6c5be0
AlexeySkillman <Alexey.skillman@gmail.com>
2022-02-21 10:55:13 +0500

    End Main Screen

--- app/src/main/java/com/example/movieapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.movieapp.screens.home
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,8 +12,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.movieapp.MovieRow
+import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
 import com.example.movieapp.navigation.MovieScreens
+import com.example.movieapp.widgets.MovieRow
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -37,16 +38,7 @@ fun HomeScreen(navController: NavController) {
 @Composable
 fun MainContent(
     navController: NavController,
-    movieList: List<String> = listOf(
-    "Daniel",
-    "Jonah",
-    "Petr",
-    "Mark",
-    "Hezekiah",
-    "Josiah",
-    "David"
-
-)) {
+    movieList: List<Movie> = getMovies()) {
     Column(
         modifier = Modifier.padding(12.dp)
     ) {
